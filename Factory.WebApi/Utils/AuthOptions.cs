c0711282567d7492b487c44eda5064893855bcf4
nytake5 <58112678+nytake5@users.noreply.github.com>
2023-05-02 21:52:03 +0400

    add auth options

---
+++ Factory.WebApi/Utils/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Factory.WebApi.Utils;
+
+public static class AuthOptions
+{
+    public const string Issuer = "MyService";
+    public const string Audience = "MyClient";
+    const string Key = "mysupersecret_secretkey!123";
+    public static readonly TimeSpan Lifetime = TimeSpan.FromMinutes(60);
+    
+    public static SymmetricSecurityKey GetSymmetricSecurityKey()
+    {
+        return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
+    }
+}
